fb068e9eb04c354873a57fc6d3511d4a9d80f2ef
chandra prakash <chandra.prakash@geminisolutions.in>
2018-06-23 17:52:12 +0530

    bookbhook funtionality

--- src/app/jarvis/chat/chat.component.ts
+++ src/app/jarvis/chat/chat.component.ts
@@ -2,11 +2,8 @@ import {
     AfterViewChecked, Component, ComponentFactoryResolver, ElementRef, OnInit, ViewChild,
     ViewContainerRef
 } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {MessageComponent} from '../message/message.component';
-// import {HttpService} from '../../services/http.service';
-// import {HeaderService} from './../../services/header.service';
-import {messageData} from "./message.dummy_data";
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageComponent } from '../message/message.component';
 
 @Component({
     selector: 'app-chat',
@@ -16,19 +13,25 @@ import {messageData} from "./message.dummy_data";
 export class ChatComponent implements OnInit, AfterViewChecked {
 
     @ViewChild('inputMessage') inputEl: ElementRef;
-    @ViewChild('container', {read: ViewContainerRef}) container;
+    @ViewChild('container', { read: ViewContainerRef }) container;
     @ViewChild('chatScroll') private myScrollContainer: ElementRef;
     public message: string;
-
+    public author: string;
+    public authorLeft: string;
+    public authorRight: string;
+    public left: boolean;
+    public heading: boolean;
 
     constructor(private router: Router, private route: ActivatedRoute, private resolver: ComponentFactoryResolver) {
-       // this.headerService.setHeader('Jarvis');
+        // this.headerService.setHeader('Jarvis');
     }
 
     ngOnInit() {
         this.addToMessagesList({
-            text: 'Hi there! Welcome to Jarvis. Please start by typing something in the input box to get results.For starters, you can try with the keyword "help".',
-            isIncoming: true
+            message: 'Hi there! Welcome to Bookbhook Chat Book Editor. Please start by typing something in the input box to get results.',
+            left: true,
+            heading: true,
+            author: ''
         });
     }
 
@@ -36,7 +39,7 @@ export class ChatComponent implements OnInit, AfterViewChecked {
         this.scrollToBottom();
     }
 
-    scrollToBottom(): void{
+    scrollToBottom(): void {
         try {
             // this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
             this.myScrollContainer.nativeElement.scrollIntoView(false);
@@ -44,36 +47,27 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     }
 
     sendMessage() {
+        debugger;
         if (this.message === '' || this.message === undefined) {
             return;
         } else {
-            this.addToMessagesList({
-                text: this.message,
-                isIncoming: false
-            });
-            if (this.message.toUpperCase() === 'DEMO') {
-                const randomMessage: any[] = messageData[Math.floor(Math.random() * messageData.length)];
-                randomMessage['isIncoming'] = true;
-                this.addToMessagesList(randomMessage);
-            } else {
-                // this.httpService.getResponseMessage(this.message).subscribe(response => {                     
-                //     if(!response){
-                //         this.addToMessagesList({
-                //             text: 'Oops. An error occured. Please try again',
-                //             isIncoming: true
-                //         });
-                //     } else if(!response['attachments']) {
-                //         response['isIncoming'] = true;                    
-                //         this.addToMessagesList({
-                //             text: "Sorry, this keyword hasn't been integrated in our system",
-                //             isIncoming: true
-                //         });
-                //     } else {
-                //         response['isIncoming'] = true;
-                //         this.addToMessagesList(response);
-                //     }           
-                // });
+            if (this.left && !this.heading) {
+                this.author = this.authorLeft;
             }
+            else {
+                this.author = this.authorRight;
+            }
+            this.addToMessagesList({
+                author: this.author,
+                message: this.message,
+                left: this.left || false,
+                heading: this.heading || false
+            });
+            // if (this.message.toUpperCase() === 'DEMO') {
+            //     const randomMessage: any[] = messageData[Math.floor(Math.random() * messageData.length)];
+            //     randomMessage['left'] = true;
+            //     this.addToMessagesList(randomMessage);
+            // }
         }
         this.message = '';
     }
@@ -82,13 +76,5 @@ export class ChatComponent implements OnInit, AfterViewChecked {
         const componentFactory = this.resolver.resolveComponentFactory(MessageComponent);
         const dynamicComponent = <MessageComponent>this.container.createComponent(componentFactory).instance;
         dynamicComponent.message = data;
-        // dynamicComponent.getButtonCommand().subscribe(command => {
-        //     if (command !== '') {
-        //         this.message = command;
-        //         this.inputEl.nativeElement.focus();
-        //     } else {
-        //         return;
-        //     }
-        // });
     }
 }
